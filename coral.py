a14b87a9472d48da1c6ca6bffd1c0328394948ac
vrveiga <141955738+vrveiga@users.noreply.github.com>
2024-11-13 19:53:24 -0300

    Ajust volume multiplier values for better differentiation

--- coral.py
+++ coral.py
@@ -54,7 +54,7 @@ velocity = [4, 7, 10,15]
 size = [60, 40, 20]  
 n_apple = [1, 2, 3] 
 base_volume_levels = [0.4, 0.6, 0.7]
-volume_multiplier = [0.5, 1, 1.5]
+volume_multiplier = [0.3, 1.1, 1.9]
 configs = [1, 1, 0, 1]
 
 
