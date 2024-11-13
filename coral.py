80b9aefa1359297487dd2fd7c8576bdafd25ed77
Petroncini <59212480+Petroncini@users.noreply.github.com>
2024-11-13 10:37:45 -0300

    Update coral.py

--- coral.py
+++ coral.py
@@ -18,10 +18,9 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import pygame
 import random
 import sys
-
-import pygame
 
 ##
 ## Game customization.
